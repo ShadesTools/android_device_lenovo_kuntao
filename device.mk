bcab2eab1a1b5a2ba947d0aed742f46e1897bd44
Murat Kozan <muratkozan350@gmail.com>
2021-10-14 22:58:29 +0300

    kuntao: Disable HW crypto for now

--- device.mk
+++ device.mk
@@ -209,10 +209,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
