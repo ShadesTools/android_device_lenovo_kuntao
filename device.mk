9ec5515f2314bcb694428592139ed422e2fa799b
Murat Kozan <muratkozan350@gmail.com>
2021-10-15 21:03:57 +0300

    kuntao: Move vndk current to vendor

--- device.mk
+++ device.mk
@@ -375,7 +375,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-ext \
     vndk-sp \
-    com.android.vndk.current
+    com.android.vndk.current.on_vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
