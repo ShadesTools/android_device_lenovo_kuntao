d8893c8ef9be75afd510520b54cf68fb19f4443a
Murat Kozan <muratkozan350@gmail.com>
2021-10-22 23:11:05 +0300

    kuntao: Remove Trust HAL

--- device.mk
+++ device.mk
@@ -385,10 +385,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
