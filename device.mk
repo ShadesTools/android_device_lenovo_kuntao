9b03693821d28fbcc636596ebeb5d4dbcd841093
Murat Kozan <muratkozan350@gmail.com>
2021-10-26 21:18:32 +0300

    kuntao: Build some 64bit impl hal variants

--- device.mk
+++ device.mk
@@ -178,7 +178,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-impl:64 \
     android.hardware.gatekeeper@1.0-service
 
 # GPS
@@ -243,7 +243,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-impl:64 \
     android.hardware.keymaster@3.0-service
 
 # Lenovo stuff
@@ -275,7 +275,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-impl-bcm:64 \
     android.hardware.nfc@1.0-service \
     libnfc \
     libnfcdummy \
